5e14e016217556937161951e29dbfa7ad238e6f3
RicTieri <tieri.riccardo@gmail.com>
2023-11-23 19:53:49 +0100

    adjustment and added end timer screen

--- js/script.js
+++ js/script.js
@@ -1,23 +1,32 @@
 const clockDisplay = document.getElementById('clock');
-const goalDay = new Date('Jan 1, 2024 00:00:00');
+const goalDay = new Date('Nov 23, 2023 19:53:00');
 
 // creare un output con intervallo di un secondo
-setInterval(function(){
-  clockDisplay.innerHTML = countDown(goalDay);
+let clock = setInterval(function () {
+  if (countDown(goalDay)) {
+    clockDisplay.innerHTML = countDown(goalDay);
+  } else {
+    clockDisplay.innerHTML = 'Happy New Year';
+    clearInterval(clock)
+  }
 },
-1000);
+  1000);
 
 
-  
+
 // calcolare quanto manca alla data prestabilita
-function countDown(dayEvent){
+function countDown(dayEvent) {
   let clockStart = new Date().getTime();
   let clockEnd = dayEvent.getTime();
   let expirationTime = clockEnd - clockStart;
 
-  let days = Math.floor(expirationTime / (1000 * 60 * 60 * 24));
-  let hours = Math.floor((expirationTime % (1000 * 60 * 60 * 24))/(1000 * 60 * 60));
-  let minutes = Math.floor((expirationTime % (1000 * 60 * 60))/(1000 * 60));
-  let seconds = Math.floor((expirationTime % (1000 * 60))/1000);
-  return days + "d " + hours + "h " + minutes + "m " + seconds + "s";
+  if (expirationTime < 0) {
+    return false
+  } else {
+    let days = Math.floor(expirationTime / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((expirationTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((expirationTime % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((expirationTime % (1000 * 60)) / 1000);
+    return days + "d " + hours + "h " + minutes + "m " + seconds + "s";
+  }
 }
