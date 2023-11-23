4e7bd7c0c004158aef03c2e6a18c89a20b239e24
RicTieri <tieri.riccardo@gmail.com>
2023-11-23 19:38:42 +0100

    added function and defined setinterval

--- js/script.js
+++ js/script.js
@@ -1,14 +1,23 @@
-// ottenere i dati di data e ora attuali
+const clockDisplay = document.getElementById('clock');
 const goalDay = new Date('Jan 1, 2024 00:00:00');
 
 // creare un output con intervallo di un secondo
-setInterval(countDown,
-1000, goalDay)
+setInterval(function(){
+  clockDisplay.innerHTML = countDown(goalDay);
+},
+1000);
 
+
+  
 // calcolare quanto manca alla data prestabilita
 function countDown(dayEvent){
   let clockStart = new Date().getTime();
   let clockEnd = dayEvent.getTime();
   let expirationTime = clockEnd - clockStart;
-  return console.log(expirationTime)
+
+  let days = Math.floor(expirationTime / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((expirationTime % (1000 * 60 * 60 * 24))/(1000 * 60 * 60));
+  let minutes = Math.floor((expirationTime % (1000 * 60 * 60))/(1000 * 60));
+  let seconds = Math.floor((expirationTime % (1000 * 60))/1000);
+  return days + "d " + hours + "h " + minutes + "m " + seconds + "s";
 }
